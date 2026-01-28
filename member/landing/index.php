fac16ccd8d2e8778f1a72ed622bb98d701ecad25
frontendplux <frontendplux@gmail.com>
2026-01-28 05:20:04 -0800

    v1

--- member/landing/index.php
+++ member/landing/index.php
@@ -4,9 +4,18 @@
         <div class="container text-center py-5">
             <h1 class="m-0"><img src="<?= $seo_configuration_data['icon'] ?>" style="width:70px; height: 70px;" alt="Logo"></h1>
         </div>
-        <div class="d-flex justify-content-center text-capitalize gap-4 pb-4" style="margin-top: -30px;">
-            <a href="" class="text-decoration-none text-dark fw-medium">home</a>
-            <a href="" class="text-decoration-none text-dark fw-medium">project</a>
+        <div class="d-flex justify-content-center text-uppercase gap-4 pb-4" style="margin-top: -30px;">
+            <?php 
+                foreach([
+                    ["name"=>"home", "link"=>"/$country/"],
+                    ["name"=>"about", "link"=>"/$country/about"],
+                    ["name"=>"services", "link"=>"/$country/services"],  
+                    ["name"=>"contact", "link"=>"/$country/contact"],
+                    ["name"=>"project", "link"=>"/$country/project"],
+                ] as $nav_item):
+            ?>
+            <a href="<?= $nav_item['link'] ?>" class="text-decoration-none text-dark fw-medium"><?= $nav_item['name'] ?></a>
+            <?php endforeach; ?>
         </div>
     </header>
 </body>
